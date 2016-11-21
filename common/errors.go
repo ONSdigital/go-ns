d864e837e194b11f9783a88e1448736eb2f0f593
Ian Kent <email@iankent.co.uk>
2016-11-21 10:32:17 +0000

    remove GetLogData

--- common/errors.go
+++ common/errors.go
@@ -1,42 +1,24 @@
 package common
 
-import (
-    "github.com/ONSdigital/go-ns/log"
-)
-
 type ONSError struct {
-    Parameters map[string]interface{}
-    RootError  error
+	Parameters map[string]interface{}
+	RootError  error
 }
 
 func NewONSError(e error, description string) *ONSError {
-    err := &ONSError{RootError: e}
-    err.AddParameter("ErrorDescription", description)
-    return err
+	err := &ONSError{RootError: e}
+	err.AddParameter("ErrorDescription", description)
+	return err
 }
 
 func (e ONSError) Error() string {
-    return e.RootError.Error()
-}
-
-func (e *ONSError) SetError() {
-    e.RootError = e
+	return e.RootError.Error()
 }
 
 func (e *ONSError) AddParameter(name string, value interface{}) *ONSError {
-    if e.Parameters == nil {
-        e.Parameters = make(map[string]interface{}, 0)
-    }
-    e.Parameters[name] = value
-    return e
-}
-
-func (e *ONSError) GetLogData() log.Data {
-    if e.Parameters == nil {
-        return log.Data{}
-    }
-    if len(e.Parameters) == 0 {
-        return log.Data{}
-    }
-    return log.Data(e.Parameters)
+	if e.Parameters == nil {
+		e.Parameters = make(map[string]interface{}, 0)
+	}
+	e.Parameters[name] = value
+	return e
 }
--- common/keyValuePair.go
+++
@@ -1,6 +0,0 @@
-package common
-
-type KVP struct {
-    Key string
-    Value interface{}
-}
