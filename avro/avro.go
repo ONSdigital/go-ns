38c509032611d467e4ab8823955fb9878d9b0406
nshumoogum <nathan.shumoogum@methods.co.uk>
2017-07-05 11:52:56 +0100

    Separate logic within avro marshal and unmarshal functions
    This should allow functions to be extended for new avro types in the
    future.

--- avro/avro.go
+++ avro/avro.go
@@ -45,13 +45,13 @@ func (schema *Schema) Marshal(s interface{}) ([]byte, error) {
 		return nil, ErrUnsupportedType(v.Kind())
 	}
 
-	//If a pointer to a struct is passed, get the type of the dereferenced object.
+	// If a pointer to a struct is passed, get the type of the dereferenced object.
 	typ := reflect.TypeOf(s)
 	if typ.Kind() == reflect.Ptr {
 		typ = typ.Elem()
 	}
 
-	//Check for unsupported interface types
+	// Check for unsupported interface types
 	err := checkFieldType(v, typ)
 	if err != nil {
 		return nil, err
@@ -62,23 +62,9 @@ func (schema *Schema) Marshal(s interface{}) ([]byte, error) {
 		return nil, err
 	}
 
-	record := avro.NewGenericRecord(avroSchema)
-
-	for i := 0; i < v.NumField(); i++ {
-		fieldTag := typ.Field(i).Tag.Get("avro")
-		if fieldTag == "-" {
-			continue
-		}
-		fieldName := typ.Field(i).Name
-
-		switch typ.Field(i).Type.Kind() {
-		case reflect.Bool:
-			value := v.FieldByName(fieldName).Bool()
-			record.Set(fieldTag, value)
-		case reflect.String:
-			value := v.FieldByName(fieldName).String()
-			record.Set(fieldTag, value)
-		}
+	record, err := getRecord(avroSchema, v, typ)
+	if err != nil {
+		return nil, err
 	}
 
 	writer := avro.NewGenericDatumWriter()
@@ -111,16 +97,96 @@ func (schema *Schema) Unmarshal(message []byte, s interface{}) error {
 		return ErrUnsupportedType(v.Kind())
 	}
 
-	decodedRecord, err := generateRecord(schema.Definition, message)
-	if err != nil {
-		log.Error(err, nil)
-		return err
-	}
-
 	// If a pointer to a struct is passed, get the type of the dereferenced object.
 	typ := reflect.TypeOf(s)
 	if typ.Kind() == reflect.Ptr {
 		typ = typ.Elem()
+	}
+
+	return populateStructFromSchema(schema.Definition, message, typ, v, vp)
+}
+
+func checkFieldType(v reflect.Value, t reflect.Type) error {
+	for i := 0; i < v.NumField(); i++ {
+		fieldTag := t.Field(i).Tag.Get("avro")
+		if fieldTag == "-" {
+			continue
+		}
+		fieldType := t.Field(i)
+
+		if !isValidType(fieldType.Type.Kind()) {
+			return ErrUnsupportedFieldType
+		}
+	}
+
+	return nil
+}
+
+func generateDecodedRecord(schema string, message []byte) (*avro.GenericRecord, error) {
+	avroSchema, err := avro.ParseSchema(schema)
+	if err != nil {
+		return nil, err
+	}
+
+	reader := avro.NewGenericDatumReader()
+	reader.SetSchema(avroSchema)
+	decoder := avro.NewBinaryDecoder(message)
+	decodedRecord := avro.NewGenericRecord(avroSchema)
+
+	err = reader.Read(decodedRecord, decoder)
+	if err != nil {
+		return nil, err
+	}
+
+	return decodedRecord, nil
+}
+
+func getRecord(avroSchema avro.Schema, v reflect.Value, typ reflect.Type) (*avro.GenericRecord, error) {
+	record := avro.NewGenericRecord(avroSchema)
+
+	for i := 0; i < v.NumField(); i++ {
+		fieldTag := typ.Field(i).Tag.Get("avro")
+		if fieldTag == "-" {
+			continue
+		}
+		fieldName := typ.Field(i).Name
+
+		switch typ.Field(i).Type.Kind() {
+		case reflect.Bool:
+			value := v.FieldByName(fieldName).Bool()
+			record.Set(fieldTag, value)
+		case reflect.String:
+			value := v.FieldByName(fieldName).String()
+			record.Set(fieldTag, value)
+		case reflect.Int32:
+			value := v.FieldByName(fieldName).Interface().(int32)
+			record.Set(fieldTag, value)
+		}
+	}
+
+	return record, nil
+}
+
+func isValidType(kind reflect.Kind) bool {
+	supportedTypes := []reflect.Kind{
+		reflect.Bool,
+		reflect.String,
+		reflect.Int32,
+	}
+
+	for _, supportedType := range supportedTypes {
+		if supportedType == kind {
+			return true
+		}
+	}
+	return false
+}
+
+func populateStructFromSchema(schema string, message []byte, typ reflect.Type, v, vp reflect.Value) error {
+	decodedRecord, err := generateDecodedRecord(schema, message)
+	if err != nil {
+		log.Error(err, nil)
+		return err
 	}
 
 	for i := 0; i < v.NumField(); i++ {
@@ -139,39 +205,4 @@ func (schema *Schema) Unmarshal(message []byte, s interface{}) error {
 	}
 
 	return nil
-}
-
-func checkFieldType(v reflect.Value, t reflect.Type) error {
-	for i := 0; i < v.NumField(); i++ {
-		fieldTag := t.Field(i).Tag.Get("avro")
-		if fieldTag == "-" {
-			continue
-		}
-		fieldType := t.Field(i)
-
-		if fieldType.Type.Kind() != reflect.String && fieldType.Type.Kind() != reflect.Bool {
-			return ErrUnsupportedFieldType
-		}
-	}
-
-	return nil
-}
-
-func generateRecord(schema string, message []byte) (*avro.GenericRecord, error) {
-	avroSchema, err := avro.ParseSchema(schema)
-	if err != nil {
-		return nil, err
-	}
-
-	reader := avro.NewGenericDatumReader()
-	reader.SetSchema(avroSchema)
-	decoder := avro.NewBinaryDecoder(message)
-	decodedRecord := avro.NewGenericRecord(avroSchema)
-
-	err = reader.Read(decodedRecord, decoder)
-	if err != nil {
-		return nil, err
-	}
-
-	return decodedRecord, nil
 }
