64657444463dc43c9688b439fa4f051f96306dc9
Geraint Edwards <gedgey@gmail.com>
2017-07-18 07:05:33 +0100

    add example program for kafka lib

---
+++ examples/cmd/examples/kafka.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/ONSdigital/dp-publish-pipeline/utils"
+	"github.com/ONSdigital/go-ns/kafka"
+	"github.com/ONSdigital/go-ns/log"
+)
+
+var (
+	tick             = time.Millisecond * 330
+	verboseTick      = false
+	maxLaunchPerTick = 20
+)
+
+func main() {
+	log.Namespace = "kafka-example"
+
+	brokers := utils.GetEnvironmentVariableAsArray("KAFKA_ADDR", "localhost:9092")
+	consumedTopic := utils.GetEnvironmentVariable("CONSUMED_TOPIC", "input")
+	producedTopic := utils.GetEnvironmentVariable("PRODUCED_TOPIC", "output")
+	maxMessageSize, err := utils.GetEnvironmentVariableInt("KAFKA_MESSAGE_SIZE", 50*1024*1024) // default to 50MB
+	if err != nil {
+		log.ErrorC("Could not create consumer", err, nil)
+		panic("Could not create consumer")
+	}
+
+	log.Info(fmt.Sprintf("Starting topics: %q -> stdout, stdin -> %q", consumedTopic, producedTopic), nil)
+
+	kafka.SetMaxMessageSize(int32(maxMessageSize))
+	producer := kafka.NewProducer(brokers, producedTopic, maxMessageSize)
+	consumer, err := kafka.NewConsumerGroup(brokers, consumedTopic, log.Namespace, kafka.OffsetNewest)
+	if err != nil {
+		log.ErrorC("Could not create consumer", err, nil)
+		panic("Could not create consumer")
+	}
+
+	exitChannel := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case consumedMessage := <-consumer.Incoming:
+				log.Info(string(consumedMessage.GetData()), nil)
+				producer.Output <- consumedMessage.GetData()
+				consumedMessage.Commit()
+			case errorMessage := <-consumer.Errors:
+				log.Error(fmt.Errorf("Aborting"), log.Data{"messageReceived": errorMessage})
+				exitChannel <- true
+				return
+			}
+		}
+	}()
+	<-exitChannel
+	log.Info("Service publish scheduler stopped", nil)
+}
---
+++ kafka/README.md
@@ -0,0 +1,6 @@
+Kafka wrapper
+=====
+
+Use channels to abstract kafka consumers and producers.
+
+See the (example source file)[../cmd/example/kafka.go] for a typical usage.
