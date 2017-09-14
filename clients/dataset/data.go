8d823faf3c9b2cd9dd887b6e941ac7818688053a
Matt Rout <matt_rout92@live.co.uk>
2017-09-14 11:16:45 +0100

    Update dataset api client to match updated spec

--- clients/dataset/data.go
+++ clients/dataset/data.go
@@ -1,15 +1,61 @@
 package dataset
 
-// Model represents a response model from the dataset api
+// Model represents a response dataset model from the dataset api
 type Model struct {
-	ID          string  `json:"id"`
-	Title       string  `json:"title"`
-	URL         string  `json:"url"`
-	ReleaseDate string  `json:"release_date"`
-	NextRelease string  `json:"next_release"`
-	Edition     string  `json:"edition"`
-	Version     string  `json:"version"`
-	Contact     Contact `json:"contact"`
+	CollectionID string    `json:"collection_id"`
+	Contact      Contact   `json:"contact"`
+	Description  string    `json:"description"`
+	Links        Links     `json:"links"`
+	NextRelease  string    `json:"next_release"`
+	Periodicity  string    `json:"yearly"`
+	Publisher    Publisher `json:"publisher"`
+	State        string    `json:"state"`
+	Theme        string    `json:"theme"`
+	Title        string    `json:"title"`
+}
+
+// Version represents a version within a dataset
+type Version struct {
+	CollectionID string `json:"collection_id"`
+	Edition      string `json:"edition"`
+	ID           string `json:"id"`
+	InstanceID   string `json:"instance_id"`
+	License      string `json:"license"`
+	Links        Links  `json:"links"`
+	ReleaseDate  string `json:"release_date"`
+	State        string `json:"date"`
+}
+
+// Edition represents an edition within a dataset
+type Edition struct {
+	Edition string `json:"edition"`
+	ID      string `json:"id"`
+	Links   Links  `json:"links"`
+	State   string `json:"state"`
+}
+
+// Publisher represents the publisher within the dataset
+type Publisher struct {
+	URL  string `json:"href"`
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
+
+// Links represent the Links within a dataset model
+type Links struct {
+	Dataset       Link `json:"dataset"`
+	Dimensions    Link `json:"dimensions"`
+	Edition       Link `json:"edition"`
+	Editions      Link `json:"editions"`
+	LatestVersion Link `json:"latest_version"`
+	Versions      Link `json:"versions"`
+	Self          Link `json:"self"`
+}
+
+// Link represents a single link within a dataset model
+type Link struct {
+	URL string `json:"href"`
+	ID  string `json:"id,omitempty"`
 }
 
 // Contact represents a response model within a dataset
@@ -17,10 +63,4 @@ type Contact struct {
 	Name      string `json:"name"`
 	Telephone string `json:"telephone"`
 	Email     string `json:"email"`
-}
-
-// Metadata represents metadata from dataset
-type Metadata struct {
-	Name        string `json:"name"`
-	Description string `json:"description"`
 }
