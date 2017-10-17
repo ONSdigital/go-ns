30b7b703551efe69bcda60c695d546bc4cd206cd
Geraint Edwards <gedgey@gmail.com>
2017-10-17 07:14:07 +0100

    WIP - still has a Sleep() in it

--- cmd/kafka-example/main.go
+++ cmd/kafka-example/main.go
@@ -5,58 +5,59 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
-	"strconv"
 
 	"context"
 	"time"
 
 	"github.com/ONSdigital/go-ns/kafka"
 	"github.com/ONSdigital/go-ns/log"
+	"github.com/kelseyhightower/envconfig"
 )
 
-const timeOut = 5 * time.Second
+type Config struct {
+	Brokers       []string      `envconfig:"KAFKA_ADDR"`
+	KafkaMaxBytes int           `envconfig:"KAFKA_MAX_BYTES"`
+	ConsumedTopic string        `envconfig:"KAFKA_CONSUMED_TOPIC"`
+	ConsumedGroup string        `envconfig:"KAFKA_CONSUMED_GROUP"`
+	ProducedTopic string        `envconfig:"KAFKA_PRODUCED_TOPIC"`
+	ConsumeMax    int           `envconfig:"KAFKA_CONSUME_MAX"`
+	KafkaSync     bool          `envconfig:"KAFKA_SYNC"`
+	TimeOut       time.Duration `envconfig:"GRACEFUL_SHUTDOWN_TIMEOUT"`
+	Chomp         bool          `envconfig:"CHOMP_MSG"`
+}
 
 func main() {
 	log.Namespace = "kafka-example"
+	cfg := &Config{
+		Brokers:       []string{"locahost:9092"},
+		KafkaMaxBytes: 50 * 1024 * 1024,
+		KafkaSync:     true,
+		ConsumedGroup: log.Namespace,
+		ConsumedTopic: "input",
+		ProducedTopic: "output",
+		ConsumeMax:    0,
+		TimeOut:       5 * time.Second,
+		Chomp:         false,
+	}
+	if err := envconfig.Process("", cfg); err != nil {
+		panic(err)
+	}
 
-	brokers := []string{os.Getenv("KAFKA_ADDR")}
-	if brokers[0] == "" {
-		brokers = []string{"localhost:9092"}
-	}
-	consumedTopic := os.Getenv("KAFKA_CONSUMED_TOPIC")
-	if consumedTopic == "" {
-		consumedTopic = "input"
-	}
-	consumedGroup := os.Getenv("KAFKA_CONSUMED_GROUP")
-	if consumedGroup == "" {
-		consumedGroup = log.Namespace
-	}
-	producedTopic := os.Getenv("KAFKA_PRODUCED_TOPIC")
-	if producedTopic == "" {
-		producedTopic = "output"
-	}
-	consumeCount := 0
-	consumeMax := 0
-	maxMessagesString := os.Getenv("KAFKA_CONSUME_MAX")
-	if maxMessagesString != "" {
-		var err error
-		consumeMax, err = strconv.Atoi(maxMessagesString)
-		if err != nil {
-			panic(err)
-		}
-	}
-	maxMessageSize := 50 * 1024 * 1024 // 50MB
+	log.Info(fmt.Sprintf("[KAFKA-TEST] Starting topics: %q -> stdout, stdin -> %q", cfg.ConsumedTopic, cfg.ProducedTopic), nil)
 
-	log.Info(fmt.Sprintf("[KAFKA-TEST] Starting topics: %q -> stdout, stdin -> %q", consumedTopic, producedTopic), nil)
-
-	kafka.SetMaxMessageSize(int32(maxMessageSize))
-	producer, err := kafka.NewProducer(brokers, producedTopic, maxMessageSize)
+	kafka.SetMaxMessageSize(int32(cfg.KafkaMaxBytes))
+	producer, err := kafka.NewProducer(cfg.Brokers, cfg.ProducedTopic, cfg.KafkaMaxBytes)
 	if err != nil {
 		log.ErrorC("[KAFKA-TEST] Could not create producer", err, nil)
 		panic("[KAFKA-TEST] Could not create producer")
 	}
 
-	consumer, err := kafka.NewConsumerGroup(brokers, consumedTopic, consumedGroup, kafka.OffsetNewest)
+	var consumer *kafka.ConsumerGroup
+	if cfg.KafkaSync {
+		consumer, err = kafka.NewSyncConsumer(cfg.Brokers, cfg.ConsumedTopic, cfg.ConsumedGroup, kafka.OffsetNewest)
+	} else {
+		consumer, err = kafka.NewConsumerGroup(cfg.Brokers, cfg.ConsumedTopic, cfg.ConsumedGroup, kafka.OffsetNewest)
+	}
 	if err != nil {
 		log.ErrorC("[KAFKA-TEST] Could not create consumer", err, nil)
 		panic("[KAFKA-TEST] Could not create consumer")
@@ -68,6 +69,7 @@ func main() {
 
 	eventLoopContext, eventLoopCancel := context.WithCancel(context.Background())
 	eventLoopDone := make(chan bool)
+	consumeCount := 0
 
 	go func(ch chan string) {
 		reader := bufio.NewReader(os.Stdin)
@@ -75,6 +77,9 @@ func main() {
 			line, err := reader.ReadString('\n')
 			if err != nil {
 				break
+			}
+			if cfg.Chomp {
+				line = line[:len(line)-1]
 			}
 			ch <- line
 		}
@@ -88,22 +93,36 @@ func main() {
 		defer close(eventLoopDone)
 		for {
 			select {
+			case <-time.After(1 * time.Second):
+				log.Trace("[KAFKA-TEST] tick", nil)
 			case <-eventLoopContext.Done():
+				log.Trace("[KAFKA-TEST] Event loop context done", log.Data{"eventLoopContextErr": eventLoopContext.Err()})
 				return
 			case consumedMessage := <-consumer.Incoming():
-				log.Info("[KAFKA-TEST] Received message", nil)
+				consumeCount++
+				log.Info("[KAFKA-TEST] Received message", log.Data{"consumeCount": consumeCount, "consumeMax": cfg.ConsumeMax})
 				consumedData := consumedMessage.GetData()
+				sleep := 500*time.Millisecond + cfg.TimeOut
 				log.Info("[KAFKA-TEST] Message consumed", log.Data{
 					"messageString": string(consumedData),
 					"messageRaw":    consumedData,
 					"messageLen":    len(consumedData),
+					"consumeCount":  consumeCount,
+					"sleep":         sleep,
 				})
+				time.Sleep(sleep)
+				log.Trace("[KAFKA-TEST] done sleeping", nil)
 				producer.Output() <- consumedData
-				consumedMessage.Commit()
-				log.Info("[KAFKA-TEST] committed message", log.Data{"messageString": string(consumedData)})
-				consumeCount++
-				if consumeCount == consumeMax {
-					log.Trace("consumed max - exiting eventLoop", nil)
+				if cfg.KafkaSync {
+					log.Trace("[KAFKA-TEST] pre-release", nil)
+					consumer.CommitAndRelease(consumedMessage)
+					log.Trace("[KAFKA-TEST] postrelease", nil)
+				} else {
+					consumedMessage.Commit()
+				}
+				log.Info("[KAFKA-TEST] committed message", log.Data{"consumeCount": consumeCount})
+				if consumeCount == cfg.ConsumeMax {
+					log.Trace("[KAFKA-TEST] consumed max - exiting eventLoop", nil)
 					return
 				}
 			case stdinLine := <-stdinChannel:
@@ -113,41 +132,46 @@ func main() {
 		}
 	}()
 
-	// block until a fatal error/signal or eventLoopDone - then proceed to shutdown
+	// block until a fatal error, signal or eventLoopDone - then proceed to shutdown
 	select {
 	case <-eventLoopDone:
 		log.Info("[KAFKA-TEST] Quitting after event loop aborted", nil)
-	case <-signals:
-		log.Info("[KAFKA-TEST] os signal received", nil)
+	case sig := <-signals:
+		log.Info("[KAFKA-TEST] Quitting after OS signal", log.Data{"signal": sig})
 	case consumerError := <-consumer.Errors():
-		log.Error(fmt.Errorf("[KAFKA-TEST] Aborting consumer"), log.Data{"messageReceived": consumerError})
+		log.ErrorC("[KAFKA-TEST] Aborting consumer", consumerError, nil)
 	case producerError := <-producer.Errors():
-		log.Error(fmt.Errorf("[KAFKA-TEST] Aborting producer"), log.Data{"messageReceived": producerError})
+		log.ErrorC("[KAFKA-TEST] Aborting producer", producerError, nil)
 	}
 
 	// give the eventLoop time to close gracefully before exiting
-	ctx, cancel := context.WithTimeout(context.Background(), timeOut)
+	ctx, cancel := context.WithTimeout(context.Background(), cfg.TimeOut)
 
 	// background graceful shutdown
 	go func() {
-		log.Info("[KAFKA-TEST] Attempting to stop listening to kafka consumer group", nil)
+		log.Info("[KAFKA-TEST] Stopping kafka consumer listener", nil)
 		consumer.StopListeningToConsumer(ctx)
-		log.Info("[KAFKA-TEST] Successfully stopped listening to kafka consumer group", nil)
+		log.Info("[KAFKA-TEST] Stopped kafka consumer listener", nil)
 		eventLoopCancel()
+		// wait for eventLoopDone: all in-flight messages have been processed
 		<-eventLoopDone
-		log.Info("[KAFKA-TEST] Attempting to close kafka producer", nil)
+		log.Info("[KAFKA-TEST] Closing kafka producer", nil)
 		producer.Close(ctx)
-		log.Info("[KAFKA-TEST] Successfully closed kafka producer", nil)
-		log.Info("[KAFKA-TEST] Attempting to close kafka consumer group", nil)
+		log.Info("[KAFKA-TEST] Closed kafka producer", nil)
+		log.Info("[KAFKA-TEST] Closing kafka consumer", nil)
 		consumer.Close(ctx)
-		log.Info("[KAFKA-TEST] Successfully closed kafka consumer group", nil)
+		log.Info("[KAFKA-TEST] Closed kafka consumer", nil)
 
-		log.Info("[KAFKA-TEST] Successfully shutdown", nil)
-		cancel() // stop timeout
+		log.Info("[KAFKA-TEST] Done shutdown - cancelling timeout context", nil)
+		cancel() // stop timer
 	}()
 
-	// wait for timeout or success
+	// wait for timeout or success (via cancel)
 	<-ctx.Done()
-	log.ErrorC("[KAFKA-TEST] shutdown done", ctx.Err(), nil)
+	if ctx.Err() == context.DeadlineExceeded {
+		log.ErrorC("[KAFKA-TEST]", ctx.Err(), nil)
+	} else {
+		log.Info("[KAFKA-TEST] Done shutdown gracefully", log.Data{"context": ctx.Err()})
+	}
 	os.Exit(1)
 }
--- kafka/consumer-group.go
+++ kafka/consumer-group.go
@@ -9,17 +9,19 @@ import (
 	"github.com/bsm/sarama-cluster"
 )
 
-var tick = time.Millisecond * 4000
+var tick = time.Millisecond * 1500
 
 // ConsumerGroup represents a Kafka consumer group instance.
 type ConsumerGroup struct {
-	consumer *cluster.Consumer
-	incoming chan Message
-	errors   chan error
-	closer   chan struct{}
-	closed   chan struct{}
-	topic    string
-	group    string
+	consumer     *cluster.Consumer
+	incoming     chan Message
+	errors       chan error
+	closer       chan struct{}
+	closed       chan struct{}
+	topic        string
+	group        string
+	sync         bool
+	upstreamDone chan bool
 }
 
 // Incoming provides a channel of incoming messages.
@@ -30,6 +32,21 @@ func (cg ConsumerGroup) Incoming() chan Message {
 // Errors provides a channel of incoming errors.
 func (cg ConsumerGroup) Errors() chan error {
 	return cg.errors
+}
+
+// Release signals that upstream has completed an incoming message
+// i.e. move on to read the next message
+func (cg ConsumerGroup) Release() {
+	cg.upstreamDone <- true
+}
+
+// Release signals the successful completion of a read incoming message
+func (cg ConsumerGroup) CommitAndRelease(msg Message) {
+	log.Trace("pre-commit", nil)
+	msg.Commit()
+	log.Trace("postCommit pre-release", nil)
+	cg.Release()
+	log.Trace("           postRelease", nil)
 }
 
 // StopListeningToConsumer stops any more messages being consumed off kafka topic
@@ -43,13 +60,12 @@ func (cg *ConsumerGroup) StopListeningToConsumer(ctx context.Context) (err error
 
 	select {
 	case <-cg.closed:
-		log.Info("Stopped listening to kafka consumer group", log.Data{"topic": cg.topic, "group": cg.group})
-		return
+		log.Info("StopListeningToConsumer saw closed kafka consumer", log.Data{"topic": cg.topic, "group": cg.group})
 	case <-ctx.Done():
 		err = fmt.Errorf("StopListeningToConsumer context timed out for group[%s] topic[%s]: %s", cg.group, cg.topic, ctx.Err())
 		log.Error(err, nil)
-		return err
 	}
+	return
 }
 
 // Close safely closes the consumer and releases all resources.
@@ -61,7 +77,7 @@ func (cg *ConsumerGroup) Close(ctx context.Context) (err error) {
 		ctx = context.Background()
 	}
 
-	// close(closer) - select avoids panic if already closed
+	// close(closer) - the select{} avoids panic if already closed (by StopListeningToConsumer)
 	select {
 	case <-cg.closer:
 	default:
@@ -76,20 +92,28 @@ func (cg *ConsumerGroup) Close(ctx context.Context) (err error) {
 		if err = cg.consumer.Close(); err != nil {
 			err = fmt.Errorf("Failed to close kafka consumer group for group[%s] topic[%s]: %s", cg.group, cg.topic, err)
 			log.Error(err, nil)
-			return
+		} else {
+			log.Info("Successfully closed kafka consumer group", log.Data{"topic": cg.topic, "group": cg.group})
 		}
-
-		log.Info("Successfully closed kafka consumer group", log.Data{"topic": cg.topic, "group": cg.group})
-		return
 	case <-ctx.Done():
 		err = fmt.Errorf("Shutdown context timed out for group[%s] topic[%s]: %s", cg.group, cg.topic, ctx.Err())
 		log.Error(err, nil)
-		return err
 	}
+	return
 }
 
-// NewConsumerGroup returns a new consumer group using default configuration.
+// NewSyncConsumer returns a new synchronous consumer group using default configuration.
+func NewSyncConsumer(brokers []string, topic string, group string, offset int64) (*ConsumerGroup, error) {
+	return newConsumer(brokers, topic, group, offset, true)
+}
+
+// NewConsumerGroup returns a new asynchronous consumer group using default configuration.
 func NewConsumerGroup(brokers []string, topic string, group string, offset int64) (*ConsumerGroup, error) {
+	return newConsumer(brokers, topic, group, offset, false)
+}
+
+// newConsumer returns a new consumer group using default configuration.
+func newConsumer(brokers []string, topic string, group string, offset int64, sync bool) (*ConsumerGroup, error) {
 
 	config := cluster.NewConfig()
 	config.Group.Return.Notifications = true
@@ -99,52 +123,98 @@ func NewConsumerGroup(brokers []string, topic string, group string, offset int64
 
 	consumer, err := cluster.NewConsumer(brokers, group, []string{topic}, config)
 	if err != nil {
-		return nil, fmt.Errorf("Bad NewConsumer of group[%s] topic[%s]: %s", group, topic, err)
+		return nil, fmt.Errorf("newConsumer error: group[%s] topic[%s]: %s", group, topic, err)
 	}
 
-	cg := ConsumerGroup{
-		consumer: consumer,
-		incoming: make(chan Message),
-		closer:   make(chan struct{}),
-		closed:   make(chan struct{}),
-		errors:   make(chan error),
-		topic:    topic,
-		group:    group,
+	var upstream chan Message
+	if sync {
+		// make the upstream channel buffered, so we can send-and-wait for upstreamDone
+		upstream = make(chan Message, 1)
+	} else {
+		upstream = make(chan Message)
 	}
 
+	cg := &ConsumerGroup{
+		consumer:     consumer,
+		incoming:     upstream,
+		closer:       make(chan struct{}),
+		closed:       make(chan struct{}),
+		errors:       make(chan error),
+		topic:        topic,
+		group:        group,
+		sync:         sync,
+		upstreamDone: make(chan bool),
+	}
+
+	// listener goroutine - listen to consumer.Messages() and upstream them
+	// if this blocks while upstreaming a message, we can shutdown consumer via the following goroutine
 	go func() {
-		log.Info(fmt.Sprintf("Started kafka consumer of topic %q group %q", cg.topic, cg.group), nil)
+		log.Info(fmt.Sprintf("Started kafka consumer listener of topic %q group %q", cg.topic, cg.group), nil)
 		defer close(cg.closed)
-		select {
-		case <-cg.closer:
-			cg.consumer.CommitOffsets()
-			log.Info(fmt.Sprintf("Closing kafka consumer of topic %q group %q", cg.topic, cg.group), nil)
-			return
-		case msg := <-cg.consumer.Messages():
-			cg.Incoming() <- SaramaMessage{msg, cg.consumer}
-		}
-	}()
-
-	go func() {
-		hasBalanced := false
-		for {
+		for looping := true; looping; {
+			log.Trace("listener.....", nil)
 			select {
 			case <-cg.closer:
-				return
+				log.Trace("listener.....CLOSER closed - exiting listener", nil)
+				looping = false
+			case <-time.After(tick):
+				log.Trace("listener.....tick", nil)
+			case msg := <-cg.consumer.Messages():
+				log.Trace("listener.....msg sending upstream, may block...", nil)
+				cg.Incoming() <- SaramaMessage{msg, cg.consumer}
+				if cg.sync {
+					// wait for msg-processed or close-consumer triggers
+					log.Trace("listener.....msg upstreamed - waiting for sync", nil)
+					for loopingForSync := true; loopingForSync; {
+						select {
+						case <-cg.upstreamDone:
+							log.Trace("listener.....sync DONE!", nil)
+							loopingForSync = false
+						// case <-cg.closer:
+						// 	log.Trace("listener.....sync closer triggered", nil)
+						// 	looping = false
+						// 	loopingForSync = false
+						case <-time.After(tick):
+							log.Trace("listener.....sync tick", nil)
+						}
+					}
+				} else {
+					log.Trace("listener.....msg upstreamed", nil)
+				}
+			}
+		}
+		cg.consumer.CommitOffsets()
+		log.Info(fmt.Sprintf("Closed kafka consumer listener of topic %q group %q", cg.topic, cg.group), nil)
+	}()
+
+	// control goroutine - allows us to close consumer even if blocked while upstreaming a message (above)
+	go func() {
+		hasBalanced := false // avoid CommitOffsets() being called before we have balanced (otherwise causes a panic)
+		for looping := true; looping; {
+			select {
+			case <-cg.closer:
+				log.Info(fmt.Sprintf("Closing kafka consumer controller of topic %q group %q", cg.topic, cg.group), nil)
+				<-cg.closed
+				looping = false
 			case err := <-cg.consumer.Errors():
+				log.Trace("controller---err msg rx", nil)
 				log.Error(err, nil)
 				cg.Errors() <- err
 			case <-time.After(tick):
+				log.Trace("controller---tick", nil)
 				if hasBalanced {
 					cg.consumer.CommitOffsets()
 				}
 			case n, more := <-cg.consumer.Notifications():
+				log.Trace("controller---note", log.Data{"n": n, "more": more})
 				if more {
 					hasBalanced = true
-					log.Trace("Rebalancing group", log.Data{"topic": cg.topic, "group": cg.group, "partitions": n.Current[topic]})
+					log.Trace("Rebalancing group", log.Data{"topic": cg.topic, "group": cg.group, "partitions": n.Current[cg.topic]})
 				}
 			}
 		}
+		log.Info(fmt.Sprintf("Closed kafka consumer controller of topic %q group %q", cg.topic, cg.group), nil)
 	}()
-	return &cg, nil
+
+	return cg, nil
 }
