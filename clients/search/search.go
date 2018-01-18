c0629e9c2ab163301d167a67d1ae5f300b5fb83c
mattrout92 <matt_rout92@live.co.uk>
2018-01-18 09:28:20 +0000

    Add a search api to send requests to search api
    - Add search api models
    - Add Healtcheck
    - Add public endpoint
    - Add test coverage

---
+++ clients/search/search.go
@@ -0,0 +1,114 @@
+package search
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/ONSdigital/go-ns/clients/clientlog"
+	"github.com/ONSdigital/go-ns/rhttp"
+)
+
+const (
+	service = "search-api"
+	limit   = 20
+	offset  = 0
+)
+
+// HTTPClient provides an interface for methods on an HTTP Client
+type HTTPClient interface {
+	Get(url string) (*http.Response, error)
+}
+
+// ErrInvalidSearchAPIResponse is returned when the search api does not respond
+// with a valid status
+type ErrInvalidSearchAPIResponse struct {
+	expectedCode int
+	actualCode   int
+	uri          string
+}
+
+// Error should be called by the user to print out the stringified version of the error
+func (e ErrInvalidSearchAPIResponse) Error() string {
+	return fmt.Sprintf("invalid response from search api - should be: %d, got: %d, path: %s",
+		e.expectedCode,
+		e.actualCode,
+		e.uri,
+	)
+}
+
+// Code returns the status code received from search api if an error is returned
+func (e ErrInvalidSearchAPIResponse) Code() int {
+	return e.actualCode
+}
+
+var _ error = ErrInvalidSearchAPIResponse{}
+
+// Client is a search api client that can be used to make requests to the server
+type Client struct {
+	cli HTTPClient
+	url string
+}
+
+// New creates a new instance of Client with a given search api url
+func New(searchAPIURL string) *Client {
+	return &Client{
+		cli: rhttp.DefaultClient,
+		url: searchAPIURL,
+	}
+}
+
+// Healthcheck calls the healthcheck endpoint on the api and alerts the caller of any errors
+func (c *Client) Healthcheck() (string, error) {
+	resp, err := c.cli.Get(c.url + "/healthcheck")
+	if err != nil {
+		return service, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return service, &ErrInvalidSearchAPIResponse{http.StatusOK, resp.StatusCode, c.url + "/healthcheck"}
+	}
+
+	return service, nil
+}
+
+// Dimension allows the searching of a dimension for a specific dimension option, optionally
+// pass in offset and limit parameters respectively
+func (c *Client) Dimension(datasetID, edition, version, name, query string, params ...int) (m *Model, err error) {
+	offset := offset
+	limit := limit
+
+	if len(params) > 0 {
+		offset = params[0]
+	}
+	if len(params) > 1 {
+		limit = params[1]
+	}
+
+	uri := fmt.Sprintf("%s/search/datasets/%s/editions/%s/versions/%s/dimensions/%s?q=%s&limit=%d&offset=%d",
+		c.url,
+		datasetID,
+		edition,
+		version,
+		name,
+		query,
+		limit,
+		offset,
+	)
+
+	clientlog.Do("searching for dataset dimension option", service, uri)
+
+	resp, err := c.cli.Get(uri)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, &ErrInvalidSearchAPIResponse{http.StatusOK, resp.StatusCode, uri}
+	}
+
+	defer resp.Body.Close()
+	err = json.NewDecoder(resp.Body).Decode(&m)
+
+	return
+}
---
+++ clients/search/search_test.go
@@ -0,0 +1,157 @@
+package search
+
+import (
+	"bytes"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/ONSdigital/go-ns/clients/search/search_mocks"
+	"github.com/ONSdigital/go-ns/rhttp"
+	"github.com/golang/mock/gomock"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestSearchUnit(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	Convey("test New creates a valid Client instance", t, func() {
+		cli := New("http://localhost:22000")
+		So(cli.url, ShouldEqual, "http://localhost:22000")
+		So(cli.cli, ShouldHaveSameTypeAs, rhttp.DefaultClient)
+	})
+
+	Convey("test Dimension Method", t, func() {
+		searchResp, err := ioutil.ReadFile("./search_mocks/search.json")
+		So(err, ShouldBeNil)
+
+		Convey("test Dimension successfully returns a model upon a 200 response from search api", func() {
+			mockCli := search_mocks.NewMockHTTPClient(mockCtrl)
+			mockCli.EXPECT().Get("http://localhost:22000/search/datasets/12345/editions/time-series/versions/1/dimensions/geography?q=Newport&limit=1&offset=1").Return(
+				&http.Response{
+					StatusCode: http.StatusOK,
+					Body:       ioutil.NopCloser(bytes.NewReader(searchResp)),
+				}, nil,
+			)
+
+			searchCli := &Client{
+				cli: mockCli,
+				url: "http://localhost:22000",
+			}
+
+			m, err := searchCli.Dimension("12345", "time-series", "1", "geography", "Newport", 1, 1)
+			So(err, ShouldBeNil)
+			So(m.Count, ShouldEqual, 1)
+			So(m.Limit, ShouldEqual, 1)
+			So(m.Offset, ShouldEqual, 0)
+			So(m.TotalCount, ShouldEqual, 1)
+			So(m.Items, ShouldHaveLength, 1)
+
+			item := m.Items[0]
+			So(item.Code, ShouldEqual, "6789")
+			So(item.DimensionOptionURL, ShouldEqual, "http://localhost:22000/datasets/12345/editions/time-series/versions/1/dimensions/geography/options/6789")
+			So(item.HasData, ShouldBeTrue)
+			So(item.Label, ShouldEqual, "Newport")
+			So(item.Matches.Label, ShouldHaveLength, 1)
+			So(item.NumberOfChildren, ShouldEqual, 3)
+
+			label := item.Matches.Label[0]
+			So(label.Start, ShouldEqual, 0)
+			So(label.End, ShouldEqual, 6)
+		})
+
+		Convey("test Dimension returns error from HTTPClient if it throws an error", func() {
+			mockCli := search_mocks.NewMockHTTPClient(mockCtrl)
+			mockCli.EXPECT().Get("http://localhost:22000/search/datasets/12345/editions/time-series/versions/1/dimensions/geography?q=Newport&limit=1&offset=1").Return(
+				nil, errors.New("client threw an error"),
+			)
+
+			searchCli := &Client{
+				cli: mockCli,
+				url: "http://localhost:22000",
+			}
+
+			m, err := searchCli.Dimension("12345", "time-series", "1", "geography", "Newport", 1, 1)
+			So(err.Error(), ShouldEqual, "client threw an error")
+			So(m, ShouldBeNil)
+		})
+
+		Convey("test Dimension returns error if HTTP Status code is not 200", func() {
+			mockCli := search_mocks.NewMockHTTPClient(mockCtrl)
+			mockCli.EXPECT().Get("http://localhost:22000/search/datasets/12345/editions/time-series/versions/1/dimensions/geography?q=Newport&limit=1&offset=1").Return(
+				&http.Response{
+					StatusCode: http.StatusBadRequest,
+					Body:       ioutil.NopCloser(bytes.NewReader(nil)),
+				}, nil,
+			)
+
+			searchCli := &Client{
+				cli: mockCli,
+				url: "http://localhost:22000",
+			}
+
+			m, err := searchCli.Dimension("12345", "time-series", "1", "geography", "Newport", 1, 1)
+			So(err.Error(), ShouldEqual, "invalid response from search api - should be: 200, got: 400, path: http://localhost:22000/search/datasets/12345/editions/time-series/versions/1/dimensions/geography?q=Newport&limit=1&offset=1")
+			So(err.(*ErrInvalidSearchAPIResponse).Code(), ShouldEqual, http.StatusBadRequest)
+			So(m, ShouldBeNil)
+		})
+	})
+
+	Convey("test Healthcheck method", t, func() {
+		Convey("test Healthcheck returns no error upon a 200 response from search api", func() {
+			mockCli := search_mocks.NewMockHTTPClient(mockCtrl)
+			mockCli.EXPECT().Get("http://localhost:22000/healthcheck").Return(
+				&http.Response{
+					StatusCode: http.StatusOK,
+				}, nil,
+			)
+
+			searchCli := &Client{
+				cli: mockCli,
+				url: "http://localhost:22000",
+			}
+
+			s, err := searchCli.Healthcheck()
+			So(err, ShouldBeNil)
+			So(s, ShouldEqual, service)
+		})
+
+		Convey("test Healthcheck returns error from HTTPClient if it throws an error", func() {
+			mockCli := search_mocks.NewMockHTTPClient(mockCtrl)
+			mockCli.EXPECT().Get("http://localhost:22000/healthcheck").Return(
+				nil, errors.New("client threw an error"),
+			)
+
+			searchCli := &Client{
+				cli: mockCli,
+				url: "http://localhost:22000",
+			}
+
+			s, err := searchCli.Healthcheck()
+			So(err.Error(), ShouldEqual, "client threw an error")
+			So(s, ShouldEqual, service)
+		})
+
+		Convey("test Dimension returns error if HTTP Status code is not 200", func() {
+			mockCli := search_mocks.NewMockHTTPClient(mockCtrl)
+			mockCli.EXPECT().Get("http://localhost:22000/healthcheck").Return(
+				&http.Response{
+					StatusCode: http.StatusInternalServerError,
+				}, nil,
+			)
+
+			searchCli := &Client{
+				cli: mockCli,
+				url: "http://localhost:22000",
+			}
+
+			s, err := searchCli.Healthcheck()
+			So(err.Error(), ShouldEqual, "invalid response from search api - should be: 200, got: 500, path: http://localhost:22000/healthcheck")
+			So(err.(*ErrInvalidSearchAPIResponse).Code(), ShouldEqual, http.StatusInternalServerError)
+			So(s, ShouldEqual, service)
+		})
+	})
+}
