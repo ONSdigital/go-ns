15689e784a91edce7f6a9c326b4bcd6820874e8f
Eleanor Deal <eleanor.deal@gmail.com>
2018-02-16 15:16:56 +0000

    Create initial event in prepare function and add unmarshaller

--- log/audit.go
+++ log/audit.go
@@ -4,77 +4,103 @@ package log
 //this either has to go in kafka or a separate package
 
 import (
+	"context"
+	"encoding/json"
 	"net/http"
 	"net/url"
 	"time"
 )
 
 type AuditEvent struct {
-	created         time.Time
-	service         string
-	namespace       string
-	requestID       string
-	user            string
-	attemptedAction string
-	responseReason  string
-	request         Request
-	response        Response
+	Created         time.Time
+	Service         string
+	Namespace       string
+	RequestID       string
+	User            string
+	AttemptedAction string
+	Outcome         string
+	Request         Request
+	ResponseStatus  int
 }
 
 type Request struct {
-	method string
-	url    *url.URL
-	params map[string]interface{}
+	Method string
+	URL    *url.URL
+	Params map[string]interface{}
 }
 
-type Response struct {
-	status int
+//
+// type Response struct {
+// 	Status int
+// }
+
+type Auditor struct {
+	Namespace string
+	TokenName string
+	//kafka vars
 }
 
-func Audit(r http.Request, responseStatus int, action, reason string) {
+func (a *Auditor) Prepare(handle func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+
+		event := &AuditEvent{
+			Request: Request{
+				Method: r.Method,
+				URL:    r.URL,
+				//params - how do we get this flexibly? read body and parse form? or just query params
+			},
+		}
+
+		if serviceToken := r.Header.Get(a.TokenName); len(serviceToken) > 0 {
+			ctx = context.WithValue(ctx, a.TokenName, serviceToken)
+			event.Service = serviceToken
+		}
+
+		if from := r.Header.Get("from"); len(from) > 0 {
+			ctx = context.WithValue(ctx, "from", from)
+			event.User = from
+		}
+
+		event.RequestID = Context(r)
+		event.Namespace = a.Namespace
+
+		eventString, err := json.Marshal(event)
+		if err != nil {
+			//we couldn't create the audit event. some input must be invalid
+			//probably bomb out of the whole handler at this point.
+			//Cancel the context? - dont want to allow an action we can't track
+		}
+
+		ctx = context.WithValue(ctx, "audit", eventString)
+
+		r.WithContext(ctx)
+		handle(w, r)
+	})
+}
+
+func (event *AuditEvent) Audit(action string, response *AuditEvent) {
 	//these things are coming from context in different ways - must be set
 	//check that setting these would happen as part of the auth stuff we've designed
 
-	namespace := r.Context().Value("namespace").(string) //this is probably just the namespace of the service? maybe this isnt context
-	service := r.Context().Value("Internal-token").(string)
-	user := r.Context().Value("from").(string) //from or origin headers? though the auth middleware may take that from header to context
+	event.AttemptedAction = action //probably set per handler - might even be the permission name that allows a thing to happen
+	event.ResponseStatus = response.ResponseStatus
+	event.Outcome = response.Outcome
 
-	id := Context(&r)
+	//do this here - worst case we didn't unmarshal the prepared event
+	//so the fields in the message are the only ones we have - best to have time to be safe
+	event.Created = time.Now()
 
-	a := AuditEvent{
-		created:         time.Now(),
-		namespace:       namespace,
-		requestID:       id,
-		attemptedAction: action, //probably set per handler - might even be the permission name that allows a thing to happen
-		responseReason:  reason,
+	Info("replace this with writing to kafka", Data{"event": event})
+}
+
+func UnmarshalAudit(input context.Context) *AuditEvent {
+	a := &AuditEvent{}
+	if s, ok := input.Value("audit").(string); ok {
+		if err := json.Unmarshal([]byte(s), a); err != nil {
+			//log the error, but continue, and we'll just return an empty audit event
+		}
 	}
 
-	var identifiable bool
-	if len(service) > 0 {
-		a.service = service
-		identifiable = true
-	}
-
-	if len(user) > 0 {
-		a.user = user
-		identifiable = true
-	}
-
-	if !identifiable {
-		//freak out/throw big error but still audit what we do know
-		//throw an error that causes the calling function to fail - do not permit this action if we cant identify?
-	}
-
-	a.request = Request{
-		method: r.Method,
-		url:    r.URL,
-		//params - how do we get this flexibly? read body and parse form? or just query params
-	}
-
-	//probably least needed
-	a.response = Response{
-		status: responseStatus,
-	}
-
-	Info("replace this with writing to kafka", Data{"event": a})
+	return a
 }
