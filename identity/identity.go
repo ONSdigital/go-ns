2eb02f4d3734799729f8ee6926d7453fe77a5868
Carl Hembrough <carl@bitwaves.co.uk>
2018-03-15 18:58:16 +0000

    Store identity in request context

--- identity/identity.go
+++ identity/identity.go
@@ -8,56 +8,108 @@ import (
 
 	"context"
 	"github.com/ONSdigital/go-ns/log"
+	"encoding/json"
+	"io/ioutil"
 )
 
 //go:generate moq -out identitytest/http_client.go -pkg identitytest . HttpClient
 
 var zebedeeURL = os.Getenv("ZEBEDEE_URL")
+var florenceHeaderKey = "X-Florence-Token"
+var authHeaderKey = "Authorization"
+var userIdentityKey = "User-Identity"
+var callerIdentityKey = "Caller-Identity"
 
 type HttpClient interface {
 	Do(ctx context.Context, req *http.Request) (*http.Response, error)
 }
 
+// Handler controls the authenticating of a request
 func Handler(doAuth bool) func(http.Handler) http.Handler {
 	return handler(doAuth, rchttp.DefaultClient)
 }
 
-// Handler controls the authenticating of a request
+type identityResponse struct {
+	Identifier string `json:"identifier"`
+}
+
 func handler(doAuth bool, cli HttpClient) func(http.Handler) http.Handler {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-			if doAuth && len(req.Header.Get("X-Florence-Token")) > 0 {
 
-				// set a default zebedee value if it isn't set
-				if len(zebedeeURL) == 0 {
-					zebedeeURL = "http://localhost:8082"
+			ctx := req.Context()
+
+			if doAuth {
+				florenceToken := req.Header.Get(florenceHeaderKey)
+				authToken := req.Header.Get(authHeaderKey)
+
+				isUserReq := len(florenceToken) > 0
+				isServiceReq := len(authToken) > 0
+
+				if isUserReq || isServiceReq {
+
+					// set a default zebedee value if it isn't set
+					if len(zebedeeURL) == 0 {
+						zebedeeURL = "http://localhost:8082"
+					}
+
+					zebReq, err := http.NewRequest("GET", zebedeeURL+"/identity", nil)
+					if err != nil {
+						log.ErrorR(req, err, nil)
+						w.WriteHeader(http.StatusInternalServerError)
+						return
+					}
+
+					if isUserReq {
+						zebReq.Header.Set(florenceHeaderKey, florenceToken)
+					} else if isServiceReq {
+						zebReq.Header.Set(authHeaderKey, authToken)
+					}
+
+					resp, err := cli.Do(ctx, zebReq)
+					if err != nil {
+						log.ErrorR(req, err, nil)
+						w.WriteHeader(http.StatusInternalServerError)
+						return
+					}
+
+					// Check to see if the florence user is authorised
+					if resp.StatusCode != http.StatusOK {
+						w.WriteHeader(resp.StatusCode)
+						return
+					}
+
+					identityResp, err := unmarshalResponse(resp)
+					if err != nil {
+						log.ErrorR(req, err, nil)
+						w.WriteHeader(http.StatusInternalServerError)
+						return
+					}
+
+					userIdentity := identityResp.Identifier
+					if !isUserReq {
+						userIdentity = req.Header.Get(userIdentityKey)
+					}
+
+					ctx = context.WithValue(ctx, userIdentityKey, userIdentity)
+					ctx = context.WithValue(ctx, callerIdentityKey, identityResp.Identifier)
 				}
-
-				zebReq, err := http.NewRequest("GET", zebedeeURL+"/permission", nil)
-				if err != nil {
-					log.ErrorR(req, err, nil)
-					w.WriteHeader(http.StatusInternalServerError)
-					return
-				}
-
-				zebReq.Header.Set("X-Florence-Token", req.Header.Get("X-Florence-Token"))
-
-				resp, err := cli.Do(req.Context(), zebReq)
-				if err != nil {
-					log.ErrorR(req, err, nil)
-					w.WriteHeader(http.StatusInternalServerError)
-					return
-				}
-
-				// Check to see if the florence user is authorised
-				if resp.StatusCode != http.StatusOK {
-					w.WriteHeader(resp.StatusCode)
-					return
-				}
-
 			}
 
-			h.ServeHTTP(w, req)
+			h.ServeHTTP(w, req.WithContext(ctx))
 		})
 	}
+}
+
+func unmarshalResponse(resp *http.Response) (*identityResponse, error) {
+
+	var identityResp *identityResponse
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return identityResp, err
+	}
+	defer resp.Body.Close()
+
+	return identityResp, json.Unmarshal(b, &identityResp)
 }
--- identity/identity_test.go
+++ identity/identity_test.go
@@ -8,16 +8,24 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"bytes"
+	"io/ioutil"
+	"encoding/json"
 )
 
 var url = "/whatever"
+var florenceToken = "123"
+var authToken = "345"
+var serviceIdentifier = "api1"
+var userIdentifier = "fred@ons.gov.uk"
+var expectedZebedeeUrl = "http://localhost:8082/identity"
 
 func TestHandler_NoAuth(t *testing.T) {
 
 	Convey("Given an instance of identity handler with doAuth set to false", t, func() {
 
 		doAuth := false
-		request := httptest.NewRequest("GET", url, nil)
+		req := httptest.NewRequest("GET", url, nil)
 		responseRecorder := httptest.NewRecorder()
 
 		httpClient := &identitytest.HttpClientMock{
@@ -35,7 +43,7 @@ func TestHandler_NoAuth(t *testing.T) {
 
 		Convey("When ServeHTTP is called", func() {
 
-			identityHandler.ServeHTTP(responseRecorder, request)
+			identityHandler.ServeHTTP(responseRecorder, req)
 
 			Convey("Then the downstream HTTP handler is called", func() {
 				So(handlerCalled, ShouldBeTrue)
@@ -49,7 +57,7 @@ func TestHandler_NoHeaders(t *testing.T) {
 	Convey("Given a http request with no headers", t, func() {
 
 		doAuth := true
-		request := httptest.NewRequest("GET", url, nil)
+		req := httptest.NewRequest("GET", url, nil)
 		responseRecorder := httptest.NewRecorder()
 
 		httpClient := &identitytest.HttpClientMock{
@@ -67,49 +75,7 @@ func TestHandler_NoHeaders(t *testing.T) {
 
 		Convey("When ServeHTTP is called", func() {
 
-			identityHandler.ServeHTTP(responseRecorder, request)
-
-			Convey("Then the downstream HTTP handler is called", func() {
-				So(handlerCalled, ShouldBeTrue)
-			})
-		})
-	})
-}
-
-func TestHandler_florenceToken(t *testing.T) {
-
-	Convey("Given a request with a florence token, and mock client that returns 200", t, func() {
-
-		doAuth := true
-		request := httptest.NewRequest("GET", url, nil)
-		request.Header = map[string][]string{
-			"X-Florence-Token": {"123"},
-		}
-		responseRecorder := httptest.NewRecorder()
-
-		httpClient := &identitytest.HttpClientMock{
-			DoFunc: func(ctx context.Context, req *http.Request) (*http.Response, error) {
-				return &http.Response{
-					StatusCode: http.StatusOK,
-				}, nil
-			},
-		}
-
-		handlerCalled := false
-		httpHandler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-			handlerCalled = true
-		})
-
-		identityHandler := handler(doAuth, httpClient)(httpHandler)
-
-		Convey("When ServeHTTP is called", func() {
-
-			identityHandler.ServeHTTP(responseRecorder, request)
-
-			Convey("Then the identity service is called as expected", func() {
-				So(len(httpClient.DoCalls()), ShouldEqual, 1)
-				So(httpClient.DoCalls()[0].Req.URL.String(), ShouldEqual, "http://localhost:8082/permission")
-			})
+			identityHandler.ServeHTTP(responseRecorder, req)
 
 			Convey("Then the downstream HTTP handler is called", func() {
 				So(handlerCalled, ShouldBeTrue)
@@ -123,9 +89,9 @@ func TestHandler_IdentityServiceError(t *testing.T) {
 	Convey("Given a request with a florence token, and mock client that returns an error", t, func() {
 
 		doAuth := true
-		request := httptest.NewRequest("GET", url, nil)
-		request.Header = map[string][]string{
-			"X-Florence-Token": {"123"},
+		req := httptest.NewRequest("GET", url, nil)
+		req.Header = map[string][]string{
+			florenceHeaderKey: {florenceToken},
 		}
 		responseRecorder := httptest.NewRecorder()
 
@@ -144,11 +110,11 @@ func TestHandler_IdentityServiceError(t *testing.T) {
 
 		Convey("When ServeHTTP is called", func() {
 
-			identityHandler.ServeHTTP(responseRecorder, request)
+			identityHandler.ServeHTTP(responseRecorder, req)
 
 			Convey("Then the identity service is called as expected", func() {
 				So(len(httpClient.DoCalls()), ShouldEqual, 1)
-				So(httpClient.DoCalls()[0].Req.URL.String(), ShouldEqual, "http://localhost:8082/permission")
+				So(httpClient.DoCalls()[0].Req.URL.String(), ShouldEqual, expectedZebedeeUrl)
 			})
 
 			Convey("Then the response code is set as expected", func() {
@@ -167,9 +133,9 @@ func TestHandler_IdentityServiceErrorResponseCode(t *testing.T) {
 	Convey("Given a request with a florence token, and mock client that returns a non 200 response", t, func() {
 
 		doAuth := true
-		request := httptest.NewRequest("GET", url, nil)
-		request.Header = map[string][]string{
-			"X-Florence-Token": {"123"},
+		req := httptest.NewRequest("GET", url, nil)
+		req.Header = map[string][]string{
+			florenceHeaderKey: {florenceToken},
 		}
 		responseRecorder := httptest.NewRecorder()
 
@@ -190,11 +156,11 @@ func TestHandler_IdentityServiceErrorResponseCode(t *testing.T) {
 
 		Convey("When ServeHTTP is called", func() {
 
-			identityHandler.ServeHTTP(responseRecorder, request)
+			identityHandler.ServeHTTP(responseRecorder, req)
 
 			Convey("Then the identity service is called as expected", func() {
 				So(len(httpClient.DoCalls()), ShouldEqual, 1)
-				So(httpClient.DoCalls()[0].Req.URL.String(), ShouldEqual, "http://localhost:8082/permission")
+				So(httpClient.DoCalls()[0].Req.URL.String(), ShouldEqual, expectedZebedeeUrl)
 			})
 
 			Convey("Then the response code is the same as returned from the identity service", func() {
@@ -203,6 +169,249 @@ func TestHandler_IdentityServiceErrorResponseCode(t *testing.T) {
 
 			Convey("Then the downstream HTTP handler is not called", func() {
 				So(handlerCalled, ShouldBeFalse)
+			})
+		})
+	})
+}
+
+func TestHandler_florenceToken(t *testing.T) {
+
+	Convey("Given a request with a florence token, and mock client that returns 200", t, func() {
+
+		doAuth := true
+		req := httptest.NewRequest("GET", url, nil)
+		req.Header = map[string][]string{
+			florenceHeaderKey: {florenceToken},
+		}
+		responseRecorder := httptest.NewRecorder()
+
+		httpClient := &identitytest.HttpClientMock{
+			DoFunc: func(ctx context.Context, req *http.Request) (*http.Response, error) {
+
+				response := &identityResponse{
+					Identifier: userIdentifier,
+				}
+
+				body, _ := json.Marshal(response)
+				reader := bytes.NewBuffer(body)
+				readCloser := ioutil.NopCloser(reader)
+
+				return &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       readCloser,
+				}, nil
+			},
+		}
+
+		handlerCalled := false
+		var handlerReq *http.Request
+		httpHandler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			handlerReq = req
+			handlerCalled = true
+		})
+
+		identityHandler := handler(doAuth, httpClient)(httpHandler)
+
+		Convey("When ServeHTTP is called", func() {
+
+			identityHandler.ServeHTTP(responseRecorder, req)
+
+			Convey("Then the identity service is called as expected", func() {
+				So(len(httpClient.DoCalls()), ShouldEqual, 1)
+				zebedeeReq := httpClient.DoCalls()[0].Req
+				So(zebedeeReq.URL.String(), ShouldEqual, expectedZebedeeUrl)
+				So(zebedeeReq.Header[florenceHeaderKey][0], ShouldEqual, florenceToken)
+			})
+
+			Convey("Then the downstream HTTP handler is called", func() {
+				So(handlerCalled, ShouldBeTrue)
+			})
+
+			Convey("Then the downstream HTTP handler request has the expected context values", func() {
+				So(handlerReq.Context().Value(callerIdentityKey), ShouldEqual, userIdentifier)
+				So(handlerReq.Context().Value(userIdentityKey), ShouldEqual, userIdentifier)
+			})
+		})
+	})
+}
+
+func TestHandler_InvalidIdentityResponse(t *testing.T) {
+
+	Convey("Given a request with a florence token, and mock client that returns invalid response JSON", t, func() {
+
+		doAuth := true
+		req := httptest.NewRequest("GET", url, nil)
+		req.Header = map[string][]string{
+			florenceHeaderKey: {florenceToken},
+		}
+		responseRecorder := httptest.NewRecorder()
+
+		httpClient := &identitytest.HttpClientMock{
+			DoFunc: func(ctx context.Context, req *http.Request) (*http.Response, error) {
+
+
+				reader := bytes.NewBufferString("{ invalid JSON")
+				readCloser := ioutil.NopCloser(reader)
+
+				return &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       readCloser,
+				}, nil
+			},
+		}
+
+		handlerCalled := false
+		var handlerReq *http.Request
+		httpHandler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			handlerReq = req
+			handlerCalled = true
+		})
+
+		identityHandler := handler(doAuth, httpClient)(httpHandler)
+
+		Convey("When ServeHTTP is called", func() {
+
+			identityHandler.ServeHTTP(responseRecorder, req)
+
+			Convey("Then the identity service is called as expected", func() {
+				So(len(httpClient.DoCalls()), ShouldEqual, 1)
+				zebedeeReq := httpClient.DoCalls()[0].Req
+				So(zebedeeReq.URL.String(), ShouldEqual, expectedZebedeeUrl)
+				So(zebedeeReq.Header[florenceHeaderKey][0], ShouldEqual, florenceToken)
+			})
+
+			Convey("Then the response code is set as expected", func() {
+				So(responseRecorder.Code, ShouldEqual, http.StatusInternalServerError)
+			})
+
+			Convey("Then the downstream HTTP handler is not called", func() {
+				So(handlerCalled, ShouldBeFalse)
+			})
+		})
+	})
+}
+
+func TestHandler_authToken(t *testing.T) {
+
+	Convey("Given a request with an auth token, and mock client that returns 200", t, func() {
+
+		doAuth := true
+		req := httptest.NewRequest("GET", url, nil)
+		req.Header = map[string][]string{
+			authHeaderKey: {authToken},
+			userIdentityKey : {userIdentifier},
+		}
+		responseRecorder := httptest.NewRecorder()
+
+		httpClient := &identitytest.HttpClientMock{
+			DoFunc: func(ctx context.Context, req *http.Request) (*http.Response, error) {
+
+				response := &identityResponse{
+					Identifier: serviceIdentifier,
+				}
+
+				body, _ := json.Marshal(response)
+
+				reader := bytes.NewBuffer(body)
+				readCloser := ioutil.NopCloser(reader)
+
+				return &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       readCloser,
+				}, nil
+			},
+		}
+
+		handlerCalled := false
+		var handlerReq *http.Request
+		httpHandler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			handlerReq = req
+			handlerCalled = true
+		})
+
+		identityHandler := handler(doAuth, httpClient)(httpHandler)
+
+		Convey("When ServeHTTP is called", func() {
+
+			identityHandler.ServeHTTP(responseRecorder, req)
+
+			Convey("Then the identity service is called as expected", func() {
+				So(len(httpClient.DoCalls()), ShouldEqual, 1)
+				zebedeeReq := httpClient.DoCalls()[0].Req
+				So(zebedeeReq.URL.String(), ShouldEqual, expectedZebedeeUrl)
+				So(zebedeeReq.Header[authHeaderKey][0], ShouldEqual, authToken)
+
+			})
+
+			Convey("Then the downstream HTTP handler is called", func() {
+				So(handlerCalled, ShouldBeTrue)
+			})
+
+			Convey("Then the downstream HTTP handler request has the expected context values", func() {
+				So(handlerReq.Context().Value(callerIdentityKey), ShouldEqual, serviceIdentifier)
+				So(handlerReq.Context().Value(userIdentityKey), ShouldEqual, userIdentifier)
+			})
+		})
+	})
+}
+
+func TestHandler_bothTokens(t *testing.T) {
+
+	Convey("Given a request with both a florence token and service token", t, func() {
+
+		doAuth := true
+		req := httptest.NewRequest("GET", url, nil)
+		req.Header = map[string][]string{
+			florenceHeaderKey: {florenceToken},
+			authHeaderKey: {authToken},
+		}
+		responseRecorder := httptest.NewRecorder()
+
+		httpClient := &identitytest.HttpClientMock{
+			DoFunc: func(ctx context.Context, req *http.Request) (*http.Response, error) {
+
+				response := &identityResponse{
+					Identifier: userIdentifier,
+				}
+
+				body, _ := json.Marshal(response)
+				reader := bytes.NewBuffer(body)
+				readCloser := ioutil.NopCloser(reader)
+
+				return &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       readCloser,
+				}, nil
+			},
+		}
+
+		handlerCalled := false
+		var handlerReq *http.Request
+		httpHandler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			handlerReq = req
+			handlerCalled = true
+		})
+
+		identityHandler := handler(doAuth, httpClient)(httpHandler)
+
+		Convey("When ServeHTTP is called", func() {
+
+			identityHandler.ServeHTTP(responseRecorder, req)
+
+			Convey("Then the identity service is called as expected", func() {
+				So(len(httpClient.DoCalls()), ShouldEqual, 1)
+				zebedeeReq := httpClient.DoCalls()[0].Req
+				So(zebedeeReq.URL.String(), ShouldEqual, expectedZebedeeUrl)
+				So(zebedeeReq.Header[florenceHeaderKey][0], ShouldEqual, florenceToken)
+			})
+
+			Convey("Then the downstream HTTP handler is called", func() {
+				So(handlerCalled, ShouldBeTrue)
+			})
+
+			Convey("Then the downstream HTTP handler request has the expected context values", func() {
+				So(handlerReq.Context().Value(userIdentityKey), ShouldEqual, userIdentifier)
+				So(handlerReq.Context().Value(callerIdentityKey), ShouldEqual, userIdentifier)
 			})
 		})
 	})
