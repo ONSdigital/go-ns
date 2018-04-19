f94c80d87b01a1e0e901c990abf83a4122625ddf
Dave Llewellyn <439837@gmail.com>
2018-04-19 14:04:54 +0100

    removed unused type

--- audit/audit.go
+++ audit/audit.go
@@ -34,8 +34,6 @@ type Event struct {
 
 type avroMarshaller func(s interface{}) ([]byte, error)
 
-type jsonUnmarshaller func(data []byte, v interface{}) error
-
 //Params key value pair for additional audit data not included in the event model.
 type Params map[string]string
 
