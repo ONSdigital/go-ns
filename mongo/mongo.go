1be0b70a206222251a1345654b3347a65b17371d
Geraint Edwards <gedgey@gmail.com>
2018-05-21 10:37:20 +0100

    add funcs for mongo queries/updates that add timestamps as requested

--- mongo/mongo.go
+++ mongo/mongo.go
@@ -6,7 +6,20 @@ import (
 	"time"
 
 	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
+
+// keep these in sync with Timestamps tags below
+const (
+	lastUpdatedKey     = "last_updated"
+	uniqueTimestampKey = "unique_timestamp"
+)
+
+// keep tags in sync with above const
+type Timestamps struct {
+	LastUpdated     time.Time           `bson:"last_updated,omitempty"     json:"last_updated,omitempty"`
+	UniqueTimestamp bson.MongoTimestamp `bson:"unique_timestamp,omitempty" json:"-"`
+}
 
 // Shutdown represents an interface to the shutdown method
 type Shutdown interface {
@@ -52,4 +65,65 @@ func Close(ctx context.Context, session *mgo.Session) error {
 	case <-ctx.Done():
 		return ctx.Err()
 	}
+}
+
+// withCurrentDate creates or adds $currentDate to updateDoc - populates that with key:val
+func withCurrentDate(updateDoc bson.M, key string, val interface{}) bson.M {
+	var currentDate bson.M
+	var ok bool
+	if currentDate, ok = updateDoc["$currentDate"].(bson.M); !ok {
+		currentDate = bson.M{}
+	}
+	switch v := val.(type) {
+	case bool, bson.M:
+		currentDate[key] = v
+	default:
+		panic("Cannot handle that type")
+	}
+	updateDoc["$currentDate"] = currentDate
+	return updateDoc
+}
+
+// WithUpdates adds all timestamps to updateDoc
+func WithUpdates(updateDoc bson.M) bson.M {
+	return WithUniqueTimestampUpdate(WithLastUpdatedUpdate(updateDoc))
+}
+
+// WithNamespacedUpdates adds all timestamps to updateDoc
+func WithNamespacedUpdates(updateDoc bson.M, prefixes []string) bson.M {
+	return WithNamespacedUniqueTimestampUpdate(WithNamespacedLastUpdatedUpdate(updateDoc, prefixes), prefixes)
+}
+
+// WithLastUpdatedUpdate adds last_updated to updateDoc
+func WithLastUpdatedUpdate(updateDoc bson.M) bson.M {
+	return withCurrentDate(updateDoc, lastUpdatedKey, true)
+}
+
+// WithNamespacedLastUpdatedUpdate adds unique timestamp to updateDoc
+func WithNamespacedLastUpdatedUpdate(updateDoc bson.M, prefixes []string) bson.M {
+	newUpdateDoc := updateDoc
+	for _, prefix := range prefixes {
+		newUpdateDoc = withCurrentDate(newUpdateDoc, prefix+lastUpdatedKey, true)
+	}
+	return newUpdateDoc
+}
+
+// WithUniqueTimestampUpdate adds unique timestamp to updateDoc
+func WithUniqueTimestampUpdate(updateDoc bson.M) bson.M {
+	return withCurrentDate(updateDoc, uniqueTimestampKey, bson.M{"$type": "timestamp"})
+}
+
+// WithNamespacedUniqueTimestampUpdate adds unique timestamp to updateDoc
+func WithNamespacedUniqueTimestampUpdate(updateDoc bson.M, prefixes []string) bson.M {
+	newUpdateDoc := updateDoc
+	for _, prefix := range prefixes {
+		newUpdateDoc = withCurrentDate(newUpdateDoc, prefix+uniqueTimestampKey, bson.M{"$type": "timestamp"})
+	}
+	return newUpdateDoc
+}
+
+// WithUniqueTimestampQuery adds unique timestamp to queryDoc
+func WithUniqueTimestampQuery(queryDoc bson.M, timestamp bson.MongoTimestamp) bson.M {
+	queryDoc[uniqueTimestampKey] = timestamp
+	return queryDoc
 }
--- mongo/mongo_test.go
+++ mongo/mongo_test.go
@@ -35,6 +35,25 @@ type Mongo struct {
 	URI        string
 }
 
+type TestModel struct {
+	State           string              `bson:"state"`
+	NewKey          int                 `bson:"new_key,omitempty"`
+	LastUpdated     time.Time           `bson:"last_updated"`
+	UniqueTimestamp bson.MongoTimestamp `bson:"unique_timestamp,omitempty"`
+}
+
+type Times struct {
+	LastUpdated     time.Time           `bson:"last_updated"`
+	UniqueTimestamp bson.MongoTimestamp `bson:"unique_timestamp,omitempty"`
+}
+
+type testNamespacedModel struct {
+	State   string `bson:"state"`
+	NewKey  int    `bson:"new_key,omitempty"`
+	Currant Times  `bson:"currant",omitempty"`
+	Nixed   Times  `bson:"nixed",omitempty"`
+}
+
 type ungraceful struct{}
 
 func (t ungraceful) shutdown(ctx context.Context, session *mgo.Session, closedChannel chan bool) {
@@ -134,6 +153,106 @@ func TestSuccessfulCloseMongoSession(t *testing.T) {
 	}
 }
 
+func TestSuccessfulMongoDates(t *testing.T) {
+	session = nil
+	if _, err := setupSession(); err != nil {
+		log.Info("mongo instance not available, skip tests", log.Data{"error": err})
+		t.FailNow()
+	}
+
+	if err := setUpTestData(session.Copy()); err != nil {
+		log.ErrorC("Failed to insert test data, skipping tests", err, nil)
+		t.FailNow()
+	}
+
+	Convey("WithUpdates adds both fields", t, func() {
+
+		Convey("check data in original state", func() {
+
+			res := TestModel{}
+
+			err := queryMongo(session.Copy(), bson.M{"_id": "1"}, &res)
+			So(err, ShouldBeNil)
+			So(res, ShouldResemble, TestModel{State: "first"})
+
+		})
+
+		Convey("check data after plain Update", func() {
+
+			res := TestModel{}
+
+			err := session.DB(Database).C(Collection).Update(bson.M{"_id": "1"}, bson.M{"$set": bson.M{"new_key": 123}})
+			So(err, ShouldBeNil)
+
+			err = queryMongo(session.Copy(), bson.M{"_id": "1"}, &res)
+			So(err, ShouldBeNil)
+			So(res, ShouldResemble, TestModel{State: "first", NewKey: 123})
+
+		})
+
+		Convey("check data with Update with new dates", func() {
+
+			testStartTime := time.Now().Truncate(time.Second)
+			res := TestModel{}
+
+			update := bson.M{"$set": bson.M{"new_key": 321}}
+			updateWithTimestamps := WithUpdates(update)
+			So(updateWithTimestamps, ShouldResemble, bson.M{"$currentDate": bson.M{"last_updated": true, "unique_timestamp": bson.M{"$type": "timestamp"}}, "$set": bson.M{"new_key": 321}})
+
+			err := session.DB(Database).C(Collection).Update(bson.M{"_id": "1"}, updateWithTimestamps)
+			So(err, ShouldBeNil)
+
+			err = queryMongo(session.Copy(), bson.M{"_id": "1"}, &res)
+			So(err, ShouldBeNil)
+			So(res.State, ShouldEqual, "first")
+			So(res.NewKey, ShouldEqual, 321)
+			So(res.LastUpdated, ShouldHappenOnOrAfter, testStartTime)
+			// extract time part
+			So(res.UniqueTimestamp<<32, ShouldBeGreaterThanOrEqualTo, testStartTime.Unix())
+
+		})
+
+		Convey("check data with Update with new Namespaced dates", func() {
+
+			// ensure this testStartTime is greater than last
+			time.Sleep(1010 * time.Millisecond)
+			testStartTime := time.Now().Truncate(time.Second)
+			res := testNamespacedModel{}
+
+			update := bson.M{"$set": bson.M{"new_key": 1234}}
+			updateWithTimestamps := WithNamespacedUpdates(update, []string{"nixed.", "currant."})
+			So(updateWithTimestamps, ShouldResemble, bson.M{
+				"$currentDate": bson.M{
+					"currant.last_updated":     true,
+					"currant.unique_timestamp": bson.M{"$type": "timestamp"},
+					"nixed.last_updated":       true,
+					"nixed.unique_timestamp":   bson.M{"$type": "timestamp"},
+				},
+				"$set": bson.M{"new_key": 1234},
+			})
+
+			err := session.DB(Database).C(Collection).Update(bson.M{"_id": "1"}, updateWithTimestamps)
+			So(err, ShouldBeNil)
+
+			err = queryNamespacedMongo(session.Copy(), bson.M{"_id": "1"}, &res)
+			So(err, ShouldBeNil)
+			So(res.State, ShouldEqual, "first")
+			So(res.NewKey, ShouldEqual, 1234)
+			So(res.Currant.LastUpdated, ShouldHappenOnOrAfter, testStartTime)
+			So(res.Nixed.LastUpdated, ShouldHappenOnOrAfter, testStartTime)
+			// extract time part
+			So(res.Currant.UniqueTimestamp<<32, ShouldBeGreaterThanOrEqualTo, testStartTime.Unix())
+			So(res.Nixed.UniqueTimestamp<<32, ShouldBeGreaterThanOrEqualTo, testStartTime.Unix())
+
+		})
+
+	})
+
+	if err := cleanupTestData(session.Copy()); err != nil {
+		log.ErrorC("Failed to delete test data", err, nil)
+	}
+}
+
 func cleanupTestData(session *mgo.Session) error {
 	defer session.Close()
 
@@ -156,26 +275,47 @@ func slowQueryMongo(session *mgo.Session) error {
 	return nil
 }
 
-func setUpTestData(session *mgo.Session) error {
+func queryMongo(session *mgo.Session, query bson.M, res *TestModel) error {
 	defer session.Close()
 
-	firstDoc := bson.M{
-		"_id":   "1",
-		"state": "first",
-	}
-
-	secondDoc := bson.M{
-		"_id":   "2",
-		"state": "second",
-	}
-
-	_, err := session.DB(Database).C(Collection).Upsert(bson.M{"_id": "1"}, firstDoc)
-	if err != nil {
+	if err := session.DB(Database).C(Collection).Find(query).One(&res); err != nil {
 		return err
 	}
 
-	_, err = session.DB(Database).C(Collection).Upsert(bson.M{"_id": "2"}, secondDoc)
-	if err != nil {
+	return nil
+}
+
+func queryNamespacedMongo(session *mgo.Session, query bson.M, res *testNamespacedModel) error {
+	defer session.Close()
+
+	if err := session.DB(Database).C(Collection).Find(query).One(&res); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getTestData() []bson.M {
+	return []bson.M{
+		bson.M{
+			"_id":   "1",
+			"state": "first",
+		},
+		bson.M{
+			"_id":   "2",
+			"state": "second",
+		},
+	}
+}
+
+func setUpTestData(session *mgo.Session) error {
+	defer session.Close()
+
+	if _, err := session.DB(Database).C(Collection).Upsert(bson.M{"_id": "1"}, getTestData()[0]); err != nil {
+		return err
+	}
+
+	if _, err := session.DB(Database).C(Collection).Upsert(bson.M{"_id": "2"}, getTestData()[1]); err != nil {
 		return err
 	}
 
