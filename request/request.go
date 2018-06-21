e3fa19f7e7714cec35cc5cf50f8a997c02e7fa12
Carl Hembrough <carl@bitwaves.co.uk>
2018-06-21 08:54:24 +0100

    Change to use log.ErrorCtx.

--- request/request.go
+++ request/request.go
@@ -5,20 +5,18 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"github.com/pkg/errors"
 )
 
 // DrainBody drains the body of the given of the given HTTP request.
-// any handler function that reads the request body should ensure that this
-// function is called.
-// ``
 func DrainBody(r *http.Request) {
 	_, err := io.Copy(ioutil.Discard, r.Body)
 	if err != nil {
-		log.InfoCtx(r.Context(), "error draining request body", nil)
+		log.ErrorCtx(r.Context(), errors.Wrap(err, "error draining request body"), nil)
 	}
 
 	err = r.Body.Close()
 	if err != nil {
-		log.InfoCtx(r.Context(), "error closing request body", nil)
+		log.ErrorCtx(r.Context(), errors.Wrap(err, "error closing request body"), nil)
 	}
 }
