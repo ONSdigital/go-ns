3c35362830769a678445e0b5b11bbfbd9d5781ec
Crispioso <crispin.merriman@gmail.com>
2018-09-14 14:49:50 +0100

    Add models to go-ns (came from geography controller)

--- clients/codelist/data.go
+++ clients/codelist/data.go
@@ -12,3 +12,52 @@ type Item struct {
 	Label string `json:"label"`
 	Name  string `json:"name"`
 }
+
+// Link contains the id and a link to a resource
+type Link struct {
+	ID   string `json:"id,omitempty"     bson:"id"`
+	Href string `json:"href"             bson:"href"`
+}
+
+// CodeListResults contains an array of code lists which can be paginated
+type CodeListResults struct {
+	Items      []CodeList `json:"items"`
+	Count      int        `json:"count"`
+	Offset     int        `json:"offset"`
+	Limit      int        `json:"limit"`
+	TotalCount int        `json:"total_count"`
+}
+
+// CodeList containing links to all possible codes
+type CodeList struct {
+	Links CodeListLinks `json:"links"`
+}
+
+// CodeListLinks contains links for a code list resource
+type CodeListLinks struct {
+	Self     *Link `json:"self"`
+	Editions *Link `json:"editions"`
+}
+
+// EditionsListResults contains an array of code lists which can be paginated
+type EditionsListResults struct {
+	Items      []EditionsList `json:"items"`
+	Count      int            `json:"count"`
+	Offset     int            `json:"offset"`
+	Limit      int            `json:"limit"`
+	TotalCount int            `json:"total_count"`
+}
+
+// EditionsList containing links to all possible codes
+type EditionsList struct {
+	Edition string           `json:"edition"`
+	Label   string           `json:"label"`
+	Links   EditionsListLink `json:"links"`
+}
+
+// EditionsListLink contains links for a code list resource
+type EditionsListLink struct {
+	Self     *Link `json:"self"`
+	Editions *Link `json:"editions"`
+	Codes    *Link `json:"codes"`
+}
