42425bb887bef077be0c0f2d01812516fa146982
Daniel W <danieldwalford@gmail.com>
2019-07-23 10:33:30 +0100

    Locale function renaming

--- common/locale.go
+++ common/locale.go
@@ -5,43 +5,45 @@ import (
 	"strings"
 )
 
-//LangEN is a reference to the english short-form localeCode "en"
+// LangEN is a reference to the english short-form localeCode "en"
 const LangEN = "en"
 
-//LangCY is a reference to the english short-form localeCode "cy"
+// LangCY is a reference to the english short-form localeCode "cy"
 const LangCY = "cy"
+
+// DefaultLang is the desired langauge to default to if requested language is unavailable
+const DefaultLang = LangEN
 
 // SetLocaleCode sets the Locale code used to set the language
 func SetLocaleCode(req *http.Request) *http.Request {
-	localeCode := ExtractLangFromSubDomain(req)
+	localeCode := GetLangFromSubDomain(req)
 
 	// Language is overridden by cookie 'lang' here if present.
 	if c, err := req.Cookie("lang"); err == nil && len(c.Value) > 0 {
-		localeCode = ExtractLangFromCookie(c)
+		localeCode = GetLangFromCookieOrDefault(c)
 	}
 	req.Header.Set("LocaleCode", localeCode)
 
 	return req
 }
 
-// ExtractLangFromSubDomain returns a language based on subdomain
-func ExtractLangFromSubDomain(req *http.Request) string {
+// GetLangFromSubDomain returns a language based on subdomain
+func GetLangFromSubDomain(req *http.Request) string {
 	args := strings.Split(req.Host, ".")
 	if len(args) == 0 {
 		// Defaulting to "en" (LangEN) if no arguments
 		return LangEN
 	}
-	if strings.Split(req.Host, ".")[0] == LangCY {
+	if args[0] == LangCY {
 		return LangCY
 	}
 	return LangEN
 }
 
-// ExtractLangFromCookie returns a language based on the lang cookie
-func ExtractLangFromCookie(c *http.Cookie) string {
+// GetLangFromCookieOrDefault returns a language based on the lang cookie or if not valid defaults it
+func GetLangFromCookieOrDefault(c *http.Cookie) string {
 	if c.Value == LangCY || c.Value == LangEN {
 		return c.Value
 	}
-	return LangEN
-
+	return DefaultLang
 }
